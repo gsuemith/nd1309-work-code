b3f2bf3d5dfa91b9a10c3a6f79767a04c90935fe
Garrick Suemith <garrick.suemith@gmail.com>
2021-05-05 20:45:00 -0400

    simple blockchain

---
+++ Project_2/simpleChain.js
@@ -0,0 +1,32 @@
+const SHA256 = require('crypto-js/sha256')
+
+// Block class
+
+class Block{
+  constructor(data){
+    this.hash = ""
+ 
+    this.height = 0
+   
+    this.body = data
+    this.time = 0
+    this.previousblockhash = ""
+  }
+}
+
+// Blockchain Class
+
+class Blockchain{
+  constructor(){
+    this.chain = [];
+    this.addBlock(new Block("First block in the chain - Genesis"))
+  }
+
+  addBlock(newBlock){
+    if(this.chain.length > 0){
+      newBlock.previousBlockHash = this.chain[this.chain.length-1].hash;
+    }
+    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString()
+    this.chain.push(newBlock);
+  }
+}
